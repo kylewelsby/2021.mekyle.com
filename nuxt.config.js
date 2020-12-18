5e84a00f51613a5f61f5faa8f1f04be65d0f0332
Kyle Welsby <kyle@mekyle.com>
2020-12-18 15:23:11 +0000

    🛠 Base path for Github Pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,13 +7,17 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: '2021.mekyle.com',
+    title: 'Top Software Engineer - Kyle Welsby',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  router: {
+    base: process.env.BASE_PATH || '/',
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
