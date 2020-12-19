2c116f08c3ef2f455a091f8460168c84fff31204
Kyle Welsby <kyle@mekyle.com>
2020-12-19 11:50:40 +0000

    💥 Initial Gridsome Commit

---
+++ gridsome.config.js
@@ -0,0 +1,46 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: 'Top Software Engineer - Kyle Welsby',
+  siteDescription:
+    'Passionate Software Engineer ranked top 1% JavaScript, top 2% Vue.js, top 5% Ruby developer in the world.',
+  icon: './src/icon.png',
+  plugins: [
+    {
+      use: 'gridsome-plugin-tailwindcss',
+    },
+    {
+      use: 'gridsome-plugin-pug',
+    },
+    // {
+    //   use: "gridsome-plugin-pwa",
+    //   options: {
+    //     disableServiceWorker: true,
+    //     title: "Software Engineer - Kyle Welsby",
+    //     display: "browser",
+    //     startUrl: "/",
+    //     themeColor: "#181818",
+    //     shortName: "Kyle Welsby"
+    //   }
+    // },
+    {
+      use: 'gridsome-source-rest',
+      options: {
+        debug: true,
+        axiosConfig: {
+          headers: {
+            'api-key': process.env.DEV_TO_API_KEY,
+          },
+        },
+        endpoint: 'https://dev.to/api/articles/me/published',
+        typeName: 'devToArticles',
+        isStatic: false,
+        isCollection: true,
+      },
+    },
+  ],
+}
---
+++ src/components/HeroText.vue
@@ -0,0 +1,46 @@
+<template lang="pug">
+  div.w-full.h-full
+    div.overflow-hidden
+      TextTicker.ticker(
+        :fontSize="16"
+        :width="1440"
+        :height="500"
+        :rows="20"
+        :speed="25"
+        color="rgba(110,108,92,1)"
+      ) {{ formattedText }}
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import TextTicker from './TextTicker.vue'
+export default Vue.extend({
+  components: {
+    TextTicker,
+  },
+  props: {
+    skills: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  data() {
+    return {
+      repeat: 20,
+      rows: 20,
+    }
+  },
+  computed: {
+    formattedText() {
+      return this.skills.join(' | ') + ' | '
+    },
+  },
+})
+</script>
+<style scoped>
+.ticker {
+  image-rendering: optimizeQuality;
+  transform: rotate(-10deg) scale(0.75);
+  transform-origin: -40% 75%;
+}
+</style>
