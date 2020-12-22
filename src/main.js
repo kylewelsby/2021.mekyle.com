0518f508de2ebb1eb32528b16a6b1a909b8a66e9
Kyle Welsby <kyle@mekyle.com>
2020-12-22 15:17:34 +0000

    🛠 String for uptime monitors to check

--- src/main.js
+++ src/main.js
@@ -5,4 +5,5 @@ import DefaultLayout from '~/layouts/Default.vue'
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
+  head.script.push({ innerHTML: `/* EOF:https://mekyle.com */`, body: true })
 }
